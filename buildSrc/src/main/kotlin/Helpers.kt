804b445be0546ba120d29f3acdb421f382bf943d
tiaga <10153548+tiaga@users.noreply.github.com>
2025-09-21 18:14:41 +0300

    build: disable including dependency metadata

--- buildSrc/src/main/kotlin/Helpers.kt
+++ buildSrc/src/main/kotlin/Helpers.kt
@@ -6,8 +6,6 @@ import java.net.http.HttpClient
 import java.net.http.HttpRequest
 import java.net.http.HttpResponse
 import java.net.URI
-import java.time.Clock.systemUTC
-import java.time.LocalDate
 import java.util.Base64
 import java.util.Properties
 import kotlin.system.exitProcess
@@ -224,6 +222,10 @@ fun Project.setupAppCommon() {
             }
         } else if (requireFlavor().contains("(GitHub|Expert|Play)Release".toRegex())) {
             exitProcess(0)
+        }
+        dependenciesInfo {
+            includeInApk = false
+            includeInBundle = false
         }
         buildTypes {
             val key = signingConfigs.findByName("release")
